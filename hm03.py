6c3ff1145c966034ef65d0098382af6396f6bf36
Wave <ert4.51c@gmail.com>
2024-03-07 16:51:03 +0200

    Task_1

--- hm03.py
+++ hm03.py
@@ -1,1 +1,15 @@
-#Перше завдання
+# Перша задача
+import datetime
+
+def get_days_from_today(date: str) -> int:
+    try:
+        # Перетворюємо рядок дати на об'єкт datetime
+        input_date = datetime.datetime.strptime(date, '%Y-%m-%d')
+        # Поточна дата
+        current_date = datetime.datetime.today()
+        # Різниця між поточною датою та заданою датою
+        days_difference = (current_date - input_date).days
+        return days_difference
+    except ValueError:
+        # Обробка винятку неправильного формату
+        return "Неправильний формат дати. Введіть дату у форматі 'РРРР-ММ-ДД'."
