646bb3755c4c27e06a06f8d077f552834c548712
Wave <ert4.51c@gmail.com>
2024-04-20 14:09:56 +0300

    задача 3

--- hm03/03.py
+++ hm03/03.py
@@ -1,4 +1,31 @@
-import random
+import re
 
-fill_percentage = random.random() * 100
-print(f"Заповнення: {fill_percentage:.2f}%")
+def normalize_phone(phone_number):
+    # Видаляємо всі символи, крім цифр та '+'
+    cleaned_number = re.sub(r'[^0-9+]', '', phone_number)
+    
+    # Якщо номер не починається з '+', додаємо міжнародний код '+38'
+    if not cleaned_number.startswith('+'):
+        if cleaned_number.startswith('380'):
+            cleaned_number = '+' + cleaned_number
+        else:
+            cleaned_number = '+38' + cleaned_number
+    
+    return cleaned_number
+
+# Приклади телефонних номерів
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+# Нормалізуємо телефонні номери
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
