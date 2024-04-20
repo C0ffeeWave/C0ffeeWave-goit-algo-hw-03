c63af81520b02d64f75602c91d34a3d0390dfde6
Wave <ert4.51c@gmail.com>
2024-04-20 13:19:38 +0300

    Друга задача

--- hm03/02.py
+++ hm03/02.py
@@ -1,5 +1,10 @@
 #Друга задача
-def get_numbers_ticket(min, max, quantity)
-    lottery_numbers = get_numbers_ticket(1, 1000, 6)
-    import random
-    random.randint(1, 1000)
+import random
+
+def get_numbers_ticket(min_value, max_value, quantity):
+    lottery_numbers = [random.randint(min_value, max_value) for _ in range(quantity)]
+    return lottery_numbers
+
+# Тест
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
