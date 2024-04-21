48a7c4c2882c9ba34cbf4f89d579b03e72b56522
Wave <ert4.51c@gmail.com>
2024-04-21 14:30:12 +0300

    Виправлено

--- hm03/02.py
+++ hm03/02.py
@@ -2,9 +2,14 @@
 import random
 
 def get_numbers_ticket(min_value, max_value, quantity):
+    # Перевірка  параметрів
+    if min_value < 1 or max_value > 1000 or quantity < 1 or quantity > (max_value - min_value + 1):
+        return []
+    # Генерація унікальних лотерейних чисел
     lottery_numbers = [random.randint(min_value, max_value) for _ in range(quantity)]
-    return lottery_numbers
+    return sorted(set(lottery_numbers))  # Видаляємо повторювані числа множиною і сортуємо
 
 # Тест
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
+
